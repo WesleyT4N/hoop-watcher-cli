1a647d5136f7348b8165f041e4125b77c920029d
Wes T4N <wesleyt1998@gmail.com>
2023-04-17 13:37:31 -0400

    add highlight query string func

---
+++ highlighs_test.go
@@ -0,0 +1,21 @@
+package hoop_watcher_test
+
+import (
+	"testing"
+	"time"
+
+	hoop_watcher "github.com/WesleyT4N/hoop-watcher-cli"
+)
+
+func TestHighlightQueryString(t *testing.T) {
+	t.Run("it generates Youtube Query string for team and date", func(t *testing.T) {
+		loc, _ := time.LoadLocation("Local")
+		date := time.Date(2023, time.January, 1, 0, 0, 0, 0, loc)
+		got := hoop_watcher.TeamHighlightQueryString("Knicks", date)
+
+		want := "Knicks NBA Highlights 2023-01-01"
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
--- highlights.go
+++ highlights.go
@@ -3,7 +3,15 @@ package hoop_watcher
 import (
 	"fmt"
 	"io"
+	"time"
 )
+
+const DAILY_DATE_FORMAT = "2006-01-02"
+
+func TeamHighlightQueryString(team string, reqTime time.Time) string {
+	dateStr := reqTime.Format(DAILY_DATE_FORMAT)
+	return fmt.Sprintf("%s NBA Highlights %s", team, dateStr)
+}
 
 func GetHighlights(team string, out io.Writer) {
 	fmt.Fprintf(out, "Getting highlights for team %s", team)
