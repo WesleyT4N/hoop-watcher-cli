001e7564474a5dc69f33e73922b9111cb42ad038
Wes T4N <wesleyt1998@gmail.com>
2023-04-17 16:13:13 -0400

    print list of highlights

--- cmd/hoop-watcher-cli/main.go
+++ cmd/hoop-watcher-cli/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
 	hoop_watcher "github.com/WesleyT4N/hoop-watcher-cli"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/googleapi/transport"
+	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
+	youtubeClient := newYoutubeClient()
 	stdout := os.Stdout
-	hoop_watcher.GetHighlights("Knicks", stdout)
+	hoop_watcher.GetHighlights("Knicks", stdout, youtubeClient)
+}
+
+func newYoutubeClient() *youtube.Service {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error occurred loading .env file")
+	}
+
+	youtubeApiKey := os.Getenv("YOUTUBE_API_KEY")
+	youtubeClient, err := youtube.New(
+		&http.Client{
+			Transport: &transport.APIKey{Key: youtubeApiKey},
+		},
+	)
+	if err != nil {
+		log.Fatal("Error occurred setting up Youtube Client")
+	}
+
+	return youtubeClient
 }
