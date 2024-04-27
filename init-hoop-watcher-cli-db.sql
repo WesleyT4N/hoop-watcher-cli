4c0d18e7a0a216bc4a4ed5f11738bff9dc9158c0
Wes Tan <wesleyt1998@gmail.com>
2024-04-27 09:57:36 -0400

    add init-db sql script

---
+++ init-hoop-watcher-cli-db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS teams(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    abbrev TEXT NOT NULL,
+    is_favorited BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS games(
+    id SERIAL PRIMARY KEY,
+    home_team_id INTEGER NOT NULL REFERENCES teams(id),
+    away_team_id INTEGER NOT NULL REFERENCES teams(id),
+    date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game_highlights(
+    id SERIAL PRIMARY KEY,
+    game_id INTEGER NOT NULL REFERENCES games(id),
+    url VARCHAR(255) NOT NULL
+);
+
